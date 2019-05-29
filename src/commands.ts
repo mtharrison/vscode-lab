996a6fbde325ac865ce1ff23670777cb221b13d3
Matt Harrison <hi@matt-harrison.com>
2019-05-29 16:46:17 +0100

    Use output channel instead

--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,33 @@
 
 import * as vscode from 'vscode'
+import * as cp from 'child_process'
 
-const terminal = vscode.window.createTerminal(`Lab test`);
+const outputChannel = vscode.window.createOutputChannel(`Lab`);
 
-export async function runLabTest(filename: string, testname: string) {
+export async function runLabTest(filepath: string, testname: string) {
 
-  terminal.sendText(`npx lab -m 20000 -a @hapi/code -v -g ${testname} ${filename}`);
-  terminal.show();
+  outputChannel.clear();
+
+  const args = [
+    'lab',
+    '-m',
+    '30000',
+    '-v',
+    '-g',
+    testname.slice(1, testname.length - 1),
+    filepath
+  ];
+
+  console.log(args);
+
+  const cmd = cp.spawn('npx', args, {
+    cwd: vscode.workspace.getWorkspaceFolder(vscode.Uri.parse(`file://${filepath}`)).uri.fsPath
+  });
+
+  cmd.stdout.on('data', (data) => outputChannel.append(data.toString()));
+  cmd.stderr.on('data', (data) => outputChannel.append(data.toString()));
+
+  cmd.on('close', (code) => outputChannel.appendLine(`exited with code ${code}`));
+
+  outputChannel.show();
 }
