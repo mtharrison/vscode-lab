fb7339a11ae0a4e7011c0afde1abb0f41b535d06
Matt Harrison <hi@matt-harrison.com>
2026-01-09 17:32:05 +0000

    include source map always

--- esbuild.js
+++ esbuild.js
@@ -9,7 +9,7 @@ async function main() {
     bundle: true,
     format: 'cjs',
     minify: production,
-    sourcemap: !production,
+    sourcemap: true,
     sourcesContent: false,
     platform: 'node',
     outfile: 'dist/extension.js',
